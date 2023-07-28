333ffcc1b5d73de85faf7240f592c4f8b3c59ed5
Abner Freitas <defreitasabner@gmail.com>
2023-07-28 11:55:43 -0300

    FileManager now remove duplicated data based in data which had same "titulo" and "url"

--- src/file_manager.py
+++ src/file_manager.py
@@ -12,6 +12,7 @@ class FileManager:
             if os.path.exists(self.__NEWS_DATA):
                 existing_dataframe = pd.read_csv(self.__NEWS_DATA)
                 dataframe = pd.concat([existing_dataframe, new_dataframe])
+                dataframe.drop_duplicates(subset=["titulo", "url"], inplace = True)
                 dataframe.to_csv(self.__NEWS_DATA, index=False)
             else:
                 new_dataframe.to_csv(self.__NEWS_DATA, index=False)
