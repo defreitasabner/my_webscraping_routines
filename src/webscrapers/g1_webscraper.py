44eaaea9f4ce177e614e8c1bf54fdf210bb8d9f3
Abner Freitas <defreitasabner@gmail.com>
2023-08-02 19:48:23 -0300

    Class NewsWebscraper elevated to superclass

--- src/webscrapers/g1_webscraper.py
+++ src/webscrapers/g1_webscraper.py
@@ -11,58 +11,25 @@ from .news_data import NewsData
 
 class G1Webscraper(NewsWebscraper):
     def __init__(self):
-        self.__PAGE_NAME = 'G1'
-        self.__BASE_URL = "https://g1.globo.com/"
-        self.__raw_data: List[BaseData] = []
-        self.__extracted_news_data: List[NewsData] = []
-    
-    @property
-    def source(self):
-        return self.__PAGE_NAME
-    
-    @property
-    def raw_data(self) -> List[Dict[str, Any]]:
-        return self.__raw_data
-
-    @property
-    def data(self) -> List[Dict[str, Any]]:
-        return [ data.to_dict() for data in self.__extracted_news_data ]
-    
-    def remove_data(self, data: BaseData):
-        if type(data) == BaseData:
-            self.__raw_data.remove(data)
+        super().__init__(page_name = 'G1', base_url = "https://g1.globo.com/")
 
     def search_for_news_on_first_page(self) -> None:
-        soup = self._get_page(self.__BASE_URL)
+        soup = self._get_page(self._BASE_URL)
         mais_lidas_section = soup.find_all(attrs={"class": "post-bastian-products__section post-mais-lidas__section"})
         middle_section_news = soup.find_all(attrs={"class": "feed-post-body"})
         for news in middle_section_news:
             try:
                 data = BaseData(
-                    source  = self.__PAGE_NAME,
+                    source  = self._PAGE_NAME,
                     title   = treat_string(news.h2.string),
-                    url     = self.__verify_url(news.a["href"])
+                    url     = self._verify_url(news.a["href"])
                 )
-                self.__raw_data.append(data)
+                self._raw_data.append(data)
             except InvalidUrlException:
                 print(f"Url inválida para: '{data.title}'")
-        print(f"Foram encontradas {len(self.__raw_data)} notícias na primeira página de {self.__PAGE_NAME}.")
+        print(f"Foram encontradas {len(self._raw_data)} notícias na primeira página de {self._PAGE_NAME}.")
 
-    def __verify_url(self, url: str) -> None:
-        if url.startswith(self.__BASE_URL):
-            return url
-        raise InvalidUrlException("Url inválida!")
-
-    def extract_news_data(self) -> None:
-        for data in self.__raw_data:
-            try:
-                news_data = self.__extract_infos_and_raw_text(data)
-                self.__extracted_news_data.append(news_data)
-            except:
-                print(f"Ocorreu um erro extraindo mais informações da notícia com título: '{data.title}'")
-        self.__raw_data.clear()
-
-    def __extract_infos_and_raw_text(self, data: BaseData) -> None:
+    def _extract_infos_and_raw_text(self, data: BaseData) -> None:
         news_page = self._get_page(data.url)
         description = treat_string(news_page.find("h2").get_text().strip())
         category = news_page.find(attrs = { "class" : "header-editoria--link" }).get_text().strip()
@@ -79,16 +46,3 @@ class G1Webscraper(NewsWebscraper):
     def __remove_figure_captions(self, text: str) -> str:
         search_pattern = re.compile("[\d][\s]de[\s][\d][\s].*[—][\s]Foto[:]")
         return re.sub(search_pattern, "", text)
-
-    def extract_editorial_page_data(self):
-        soup = self._get_page(self.__BASE_URL)
-        news = soup.find_all(attrs=self.__NEWS_CONTAINER)
-        extracted_data = []
-        for new in news:
-            data = {
-                "titulo":  new.h2.string,
-                "descricao": new.p.string,
-                "url": new.a["href"]
-            }
-        extracted_data.append(data)
-        return extracted_data
--- src/webscrapers/interfaces/news_webscraper.py
+++ src/webscrapers/interfaces/news_webscraper.py
@@ -1,14 +1,56 @@
-from abc import ABC, abstractmethod
+from typing import List, Dict, Any
+
 import requests
 from bs4 import BeautifulSoup
 
-class NewsWebscraper(ABC):
+from webscrapers.base_data import BaseData
+from webscrapers.exceptions import InvalidUrlException
+from webscrapers.news_data import NewsData
 
-    @abstractmethod
-    def search_for_news_on_first_page(self):
-        pass
+class NewsWebscraper:
+    def __init__(self, page_name: str, base_url: str):
+        self._PAGE_NAME = page_name
+        self._BASE_URL = base_url
+        self._raw_data: List[BaseData] = []
+        self._extracted_news_data: List[NewsData] = []
+    
+    @property
+    def source(self):
+        return self._PAGE_NAME
+    
+    @property
+    def raw_data(self) -> List[Dict[str, Any]]:
+        return self._raw_data
+
+    @property
+    def data(self) -> List[Dict[str, Any]]:
+        return [ data.to_dict() for data in self._extracted_news_data ]
+
+    def remove_data(self, data: BaseData):
+        if type(data) == BaseData:
+            self._raw_data.remove(data)
 
     def _get_page(self, url):
         response = requests.get(url)
         webpage = response.content.decode("utf-8")
         return BeautifulSoup(webpage, 'lxml')
+
+    def search_for_news_on_first_page(self):
+        raise NotImplementedError()
+    
+    def _verify_url(self, url: str) -> None:
+        if url.startswith(self._BASE_URL):
+            return url
+        raise InvalidUrlException("Url inválida!")
+    
+    def extract_news_data(self) -> None:
+        for data in self._raw_data:
+            try:
+                news_data = self._extract_infos_and_raw_text(data)
+                self._extracted_news_data.append(news_data)
+            except:
+                print(f"Ocorreu um erro extraindo mais informações da notícia com título: '{data.title}'")
+        self._raw_data.clear()
+
+    def _extract_infos_and_raw_text(self, data: BaseData) -> None:
+        raise NotImplementedError()
